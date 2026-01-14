37fd14c1dfad0f5ffe0536b5a6c94e8e4c05cd3f
Joker9405 <627678656brian@gmail.com>
2026-01-14 15:13:34 +0800

    Add necessary files for deployment

--- js/app.js
+++ js/app.js
@@ -1,15 +1,8 @@
 import React, { useState } from 'react';
 
-// 你可以在这里将原本的业务逻辑拆分成单独的函数或组件
 function App() {
-  const [message, setMessage] = useState('Hello, React!');
-
-  return (
-    <div>
-      <h1>{message}</h1>
-      {/* 你可以在这里增加其他组件或功能 */}
-    </div>
-  );
+  const [message, setMessage] = useState('Hello, React and GitHub!');
+  return <div>{message}</div>;
 }
 
 export default App;
