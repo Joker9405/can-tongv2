4633c471ac193cd5b0bb2b638385024a438dda90
非冰走糖 <126657963+Joker9405@users.noreply.github.com>
2026-01-14 14:36:50 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './index.css'; // 引入你的 CSS 文件
+import App from './App'; // 引入主组件
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root') // 确保这里匹配 index.html 中的 div id="root"
 );
